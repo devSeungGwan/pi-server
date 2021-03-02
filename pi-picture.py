80e1d43428510819ad2951de37e2d0119e456a55
devSeungGwan <yanhis97531@gmail.com>
2021-03-02 17:06:28 +0900

    Fix: pi-picture.py

--- pi-picture.py
+++ pi-picture.py
@@ -10,9 +10,8 @@ camera.resolution = (2592, 1944) # (64, 64) ~ (2592, 1944) px
 for i in range(10):
     time.sleep(3)
     now = datetime.datetime.now()
-    camera.capture('{}{}'.format(
-        now.strftime(
-            "%Y-%m-%d_%H_%M_%S", 
-            ".jpg"
-        )
-    ))
+
+    capture_time = now.strftime("%Y-%m-%d_%H_%M_%S")
+    capture_format = ".jpg"
+    
+    camera.capture('{}{}'.format(capture_time, capture_format))
