8197057d6cb67c256e5aab9bcb5a17efd254eeb4
devSeungGwan <yanhis97531@gmail.com>
2021-03-02 17:01:27 +0900

    Update pi-picture.py

--- pi-picture.py
+++ pi-picture.py
@@ -1,8 +1,18 @@
 import picamera
 import time
+import datetime
+import os
+
 
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944) # (64, 64) ~ (2592, 1944) px
 
-time.sleep(3)
-camera.capture('snapshot.jpg')
+for i in range(10):
+    time.sleep(3)
+    now = datetime.datetime.now()
+    camera.capture('{}{}'.format(
+        now.strftime(
+            "%Y-%m-%d_%H_%M_%S", 
+            ".jpg"
+        )
+    ))
