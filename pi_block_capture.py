4fd6e9f341007eca6b7d063a57696eac9621f173
devSeungGwan <yanhis97531@gmail.com>
2021-03-11 19:49:03 +0900

    ADD: pi_block_capture.py

--- pi_block_capture.py
+++ pi_block_capture.py
@@ -2,21 +2,29 @@ import picamera
 import time
 import datetime
 import os
+import sys
 
 def pi_picture():
     camera = picamera.PiCamera()
     camera.resolution = (800, 800) # (64, 64) ~ (2592, 1944) px
     camera.start_preview()
+    
+    capture_folder = sys.argv[2]
+
+    try:
+        os.makedirs("./{}".format(capture_folder))
+    except:
+        pass
+
 
     for i in range(100):
         time.sleep(3)
         now = datetime.datetime.now()
 
-        capture_folder = "./capture"
         capture_time = now.strftime("%Y-%m-%d_%H_%M_%S")
         capture_format = ".jpg"
 
-        camera.capture('{}/{}{}'.format(capture_folder, capture_time, capture_format))
-        print("Capture! : {}/{}{}".format(capture_folder, capture_time, capture_format))
+        camera.capture('{}/_{}{}'.format(capture_folder, i, capture_format))
+        print("Capture! : {}/_{}{}".format(capture_folder, i, capture_format))
     
     camera.close()
