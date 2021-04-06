a67608579aca1936559370109828c61048b90d29
devSeungGwan <yanhis97531@gmail.com>
2021-04-06 11:55:26 +0900

    debug: cv_view.py

--- cv_view.py
+++ cv_view.py
@@ -1,9 +1,10 @@
 import cv2
+import time
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
   ret, img = cap.read()
   if ret:
     cv2.imshow('test', img)
-    if cv2.waitkey(1) & 0xFF == 27: # esc
-      break
+    time.sleep(100)
+    break
