c9b4989dc83005a35cbf123c5e393bfa630f846e
devSeungGwan <yanhis97531@gmail.com>
2021-04-13 11:49:38 +0900

    Issue #1: module 작성

--- RPiServer.py
+++ RPiServer.py
@@ -5,13 +5,5 @@ from pi_block_capture import pi_picture
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/capture')
-class Capture(Resource):
-    def post(self):
-        capture_folder = request.json.post('capture_folder')
-        fin = pi_picture(capture_folder)
-    
-        return fin
-
 if __name__ == "__main__":
     app.run(debug=True, host="localhost")
