baa3a3f94c968580e121dc2271fc0172a10631f9
devSeungGwan <yanhis97531@gmail.com>
2021-04-13 13:04:14 +0900

    Start: Django server

--- RPiServer.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-from flask_restx import Api, Resource
-from pi_block_capture import pi_picture
-
-app = Flask(__name__)
-api = Api(app)
-
-if __name__ == "__main__":
-    app.run(debug=True, host="localhost")
