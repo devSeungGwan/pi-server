7cf8e679764eb4aace4dcee99f2549c45b77e616
devSeungGwan <yanhis97531@gmail.com>
2021-04-15 09:14:20 +0900

    Debug: argv type 지정

--- module/CameraRecording.py
+++ module/CameraRecording.py
@@ -17,18 +17,18 @@ def camera_recording(save_folder: str, record_time: int, width: int, height: int
         pass
 
 
-    # camera.start_preview()
+    camera.start_preview()
     camera.start_recording("./{}/video.h264".format(save_folder))
     sleep(record_time)
     camera.stop_recording()
-    # camera.stop_preview()
+    camera.stop_preview()
     
 
 if __name__ == "__main__":
     save_folder = sys.argv[1]
-    record_time = sys.argv[2]
-    width = sys.argv[3]
-    height = sys.argv[4]
-    frame_rate = sys.argv[5]
+    record_time = int(sys.argv[2])
+    width = int(sys.argv[3])
+    height = int(sys.argv[4])
+    frame_rate = int(sys.argv[5])
 
     camera_recording(save_folder, record_time, width, height, frame_rate)
