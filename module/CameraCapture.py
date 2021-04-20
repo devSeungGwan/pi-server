b393b03ad3bb7b83eaa6b515dd8eb2d1eba286c1
devSeungGwan <yanhis97531@gmail.com>
2021-04-20 15:09:17 +0900

    Update: CameraCapture.py

--- module/CameraCapture.py
+++ module/CameraCapture.py
@@ -8,7 +8,7 @@ import sys
 from pprint import pprint
 
 
-def camera_caputure(save_folder: str, num_of_capture: int, width: int, height: int) -> str:
+def camera_capture(width: int, height: int, ser) -> str:
     camera = picamera.PiCamera()
     camera.resolution = (width, height)  # (64, 64) ~ (2592, 1944) px
 
@@ -16,42 +16,51 @@ def camera_caputure(save_folder: str, num_of_capture: int, width: int, height: i
     start_time = now.strftime("%Y-%m-%d_%H_%M_%S")
 
     try:
-        os.makedirs("./{}".format(save_folder))
+        os.makedirs("./{}".format(start_time))
     except:
         pass
     
     log = { 
-        "save_folder": save_folder,
-        "start_time": start_time,
-        "num_of_capture": num_of_capture,
+        "save_folder": start_time,
+        "num_of_capture": 24,
         "px": camera.resolution,
     }
 
-    for i in range(num_of_capture):
-        now = datetime.datetime.now()
-        capture_time = now.strftime("%Y-%m-%d_%H_%M_%S")
-        capture_format = ".jpg"
-        file_name = "./{}/_{}{}".format(save_folder, capture_time, capture_format)
-        camera.capture(file_name)
+    ser_capture = 0
 
-        log[i + 1] = {
-            "file name": file_name,
-            "capture time": capture_time,
-        }
+    while True:
+        if ser.readable() : 
+            res = ser.readline()
+            res = res.decode().strip()
 
-        pprint(
-            "Capture! : ./{}/_{}{}".format(save_folder, capture_time, capture_format)
-        )
+            if(res == "CAPTURE"):
+                now = datetime.datetime.now()
+                capture_time = now.strftime("%Y-%m-%d_%H_%M_%S")
+                capture_format = ".jpg"
+                file_name = "./{}/_{}{}".format(start_time, capture_time, capture_format)
+                camera.capture(file_name)
+
+                log[ser_capture + 1] = {
+                    "file name": file_name,
+                    "capture time": capture_time,
+                }
+
+                ser_capture += 1
+                
+                pprint(
+                    "Capture! : ./{}/_{}{}".format(start_time, capture_time, capture_format)
+                )  
+                
+            elif(res == "END"):
+                break     
 
     camera.close()
 
     return log
 
 if __name__ == "__main__":
-    # save_folder = str(sys.argv[1])
-    # num_of_folder = int(sys.argv[2])
-    # width = int(sys.argv[3])
-    # height = int(sys.argv[4])
+    width = int(sys.argv[1])
+    height = int(sys.argv[2])
 
     ser = serial.Serial("/dev/ttyUSB0",9600)
     while True:
@@ -59,4 +68,5 @@ if __name__ == "__main__":
             res = ser.readline()
             res = res.decode().strip()
 
-            print(res)
+            if(res == "START"):
+                camera_capture(width, height, ser)
